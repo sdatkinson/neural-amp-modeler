915e53ae646633295dc9ff8f76fc2de0840a2448
Steven Atkinson <steven@atkinson.mn>
2022-06-11 12:41:25 -0700

    DC loss

--- nam/models/base.py
+++ nam/models/base.py
@@ -24,6 +24,7 @@ class Model(pl.LightningModule, InitializableFromConfig):
         net,
         optimizer_config: Optional[dict] = None,
         scheduler_config: Optional[dict] = None,
+        loss_config: Optional[dict] = None,
     ):
         """
         :param scheduler_config: contains
@@ -39,6 +40,7 @@ class Model(pl.LightningModule, InitializableFromConfig):
         self._net = net
         self._optimizer_config = {} if optimizer_config is None else optimizer_config
         self._scheduler_config = scheduler_config
+        self._loss_config = {} if loss_config is None else loss_config
 
     @classmethod
     def init_from_config(cls, config):
@@ -52,6 +54,33 @@ class Model(pl.LightningModule, InitializableFromConfig):
 
     @classmethod
     def parse_config(cls, config):
+        """
+        e.g. 
+        
+        {
+            "net": {
+                "name": "ConvNet",
+                "config": {...}
+            },
+            "loss": {
+                "dc_weight": 0.1
+            },
+            "optimizer": {
+                "lr": 0.0003
+            },
+            "lr_scheduler": {
+                "class": "ReduceLROnPlateau",
+                "kwargs": {
+                    "factor": 0.8,
+                    "patience": 10,
+                    "cooldown": 15,
+                    "min_lr": 1e-06,
+                    "verbose": true
+                },
+                "monitor": "val_loss"
+            }
+        }    
+        """
         config = super().parse_config(config)
         net_config = config["net"]
         net = {
@@ -63,6 +92,7 @@ class Model(pl.LightningModule, InitializableFromConfig):
             "net": net,
             "optimizer_config": config["optimizer"],
             "scheduler_config": config["lr_scheduler"],
+            "loss_config": config.get("loss"),
         }
 
     @property
@@ -89,12 +119,33 @@ class Model(pl.LightningModule, InitializableFromConfig):
     def _shared_step(self, batch):
         args, targets = batch[:-1], batch[-1]
         preds = self(*args, pad_start=False)
-        return nn.MSELoss()(preds, targets)
+
+        return preds, targets
 
     def training_step(self, batch, batch_idx):
-        return self._shared_step(batch)
+        preds, targets = self._shared_step(batch)
+
+        loss = 0.0
+        # Prediction aka MSE aka "ESR" loss
+        loss = loss + self._mse_loss(preds, targets)
+
+        # DC loss
+        dc_weight = self._loss_config.get("dc_weight", 0.0)
+        if dc_weight > 0.0:
+            # Denominator could be a bad idea. I'm going to omit it esp since I'm
+            # using mini batches
+            mean_dims = torch.arange(1, preds.ndim).tolist()
+            dc_loss = nn.MSELoss()(
+                preds.mean(dim=mean_dims), targets.mean(dim=mean_dims)
+            )
+            loss = loss + dc_weight * dc_loss
+        return loss
 
     def validation_step(self, batch, batch_idx):
-        val_loss = self._shared_step(batch)
+        preds, targets = self._shared_step(batch)
+        val_loss = self._mse_loss(preds, targets)
         self.log_dict({"val_loss": val_loss})
         return val_loss
+
+    def _mse_loss(self, preds, targets):
+        return nn.MSELoss()(preds, targets)
