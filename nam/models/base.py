a988172aada633199a2621154e8d3e9712036a4f
Steven Atkinson <steven@atkinson.mn>
2022-09-03 14:45:05 -0700

    Merge dev into lstm-tweaks

--- nam/models/base.py
+++ nam/models/base.py
@@ -3,7 +3,11 @@
 # Author: Steven Atkinson (steven@atkinson.mn)
 
 """
-Lightning stuff
+Implements the base PyTorch Lightning model.
+This is meant to combine an acutal model (subclassed from `._base.BaseNet` or 
+`._base.ParametricBaseNet`) along with loss function boilerplate.
+
+For the base *PyTorch* model containing the actual architecture, see `._base`.
 """
 
 from dataclasses import dataclass
@@ -19,8 +23,7 @@ from .conv_net import ConvNet
 from .linear import Linear
 from .parametric.catnets import CatLSTM, CatWaveNet
 from .parametric.hyper_net import HyperConvNet
-from .rcnn import RCNN
-from .recurrent import LSTM, SkippyLSTM
+from .recurrent import LSTM
 from .wavenet import WaveNet
 
 
@@ -154,8 +157,6 @@ class Model(pl.LightningModule, InitializableFromConfig):
             "HyperConvNet": HyperConvNet.init_from_config,
             "Linear": Linear.init_from_config,
             "LSTM": LSTM.init_from_config,
-            "RCNN": RCNN.init_from_config,
-            "SkippyLSTM": SkippyLSTM.init_from_config,
             "WaveNet": WaveNet.init_from_config,
         }[net_config["name"]](net_config["config"])
         loss_config = LossConfig.init_from_config(config.get("loss", {}))
