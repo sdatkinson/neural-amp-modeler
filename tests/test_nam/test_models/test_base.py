81de42b263af04ca23ddcef17445bda8bd16befb
Steven Atkinson <steven@atkinson.mn>
2023-03-24 17:19:39 -0700

    Refactor

--- tests/test_nam/test_models/test_base.py
+++ tests/test_nam/test_models/test_base.py
@@ -11,41 +11,41 @@ import torch
 
 from nam.models._base import BaseNet
 
+
+class _Mock(BaseNet):
+    def __init__(self, gain: float, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.gain = gain
+
+    @property
+    def pad_start_default(self) -> bool:
+        return True
+
+    @property
+    def receptive_field(self) -> int:
+        return 1
+
+    def export_cpp_header(self, filename: Path):
+        pass
+
+    def _export_config(self):
+        pass
+
+    def _export_weights(self) -> np.ndarray:
+        pass
+
+    def _forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.gain * x
+
+
 def test_loudness():
-    class C(BaseNet):
-        def __init__(self, gain: float, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.gain = gain
-
-        @property
-        def pad_start_default(self) -> bool:
-            return True
-
-        @property
-        def receptive_field(self) -> int:
-            return 1
-        
-        def export_cpp_header(self, filename: Path):
-            pass
-        
-        def _export_config(self):
-            pass
-
-        def _export_weights(self) -> np.ndarray:
-            pass
-
-        def _forward(self, x: torch.Tensor) -> torch.Tensor:
-            return self.gain * x
-        
-    obj = C(1.0)
+    obj = _Mock(1.0)
     y = obj._loudness()
     obj.gain = 2.0
     y2 = obj._loudness()
     assert isinstance(y, float)
     # 2x louder = +6dB
     assert y2 == pytest.approx(y + 20.0 * math.log10(2.0))
-
-
 
 
 if __name__ == "__main__":
