aa8845f3158012470fdea2b395d194a18e0e12aa
Steven Atkinson <steven@atkinson.mn>
2023-04-29 13:03:36 -0700

    Black

--- tests/test_nam/test_train/test_version.py
+++ tests/test_nam/test_train/test_version.py
@@ -4,17 +4,19 @@
 
 from nam.train import _version
 
+
 def test_eq():
-    assert _version.Version(0,0,0) == _version.Version(0,0,0)
-    assert _version.Version(0,0,0) != _version.Version(0,0,1)
-    assert _version.Version(0,0,0) != _version.Version(0,1,0)
-    assert _version.Version(0,0,0) != _version.Version(1,0,0)
+    assert _version.Version(0, 0, 0) == _version.Version(0, 0, 0)
+    assert _version.Version(0, 0, 0) != _version.Version(0, 0, 1)
+    assert _version.Version(0, 0, 0) != _version.Version(0, 1, 0)
+    assert _version.Version(0, 0, 0) != _version.Version(1, 0, 0)
+
 
 def test_lt():
-    assert _version.Version(0,0,0) < _version.Version(0,0,1)
-    assert _version.Version(0,0,0) < _version.Version(0,1,0)
-    assert _version.Version(0,0,0) < _version.Version(1,0,0)
-    
-    assert _version.Version(1,2,3) < _version.Version(2,0,0)
+    assert _version.Version(0, 0, 0) < _version.Version(0, 0, 1)
+    assert _version.Version(0, 0, 0) < _version.Version(0, 1, 0)
+    assert _version.Version(0, 0, 0) < _version.Version(1, 0, 0)
 
-    assert not _version.Version(1,2,3) < _version.Version(0,4,5)
+    assert _version.Version(1, 2, 3) < _version.Version(2, 0, 0)
+
+    assert not _version.Version(1, 2, 3) < _version.Version(0, 4, 5)
