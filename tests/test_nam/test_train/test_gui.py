35737c860183ecc2cd01cf16e5483eeecae567e8
Steven Atkinson <steven@atkinson.mn>
2024-05-24 11:11:47 -0700

    xvfb for GitHub testing

--- tests/test_nam/test_train/test_gui.py
+++ tests/test_nam/test_train/test_gui.py
@@ -2,18 +2,11 @@
 # Created Date: Friday May 24th 2024
 # Author: Steven Atkinson (steven@atkinson.mn)
 
-import os
 import tkinter as tk
-from time import time
-
 
 import pytest
 
 from nam.train import gui
-
-# Required for tests on GitHub:
-if os.environ.get("DISPLAY", "") == "":
-    os.environ["DISPLAY"] = ":0.0"
 
 
 class TestPathButton(object):
