13d69072bf30161954f8fb94680e1377823c60c9
Steven Atkinson <steven@atkinson.mn>
2024-07-27 12:59:15 -0700

    Remove bin/train/main.py Move bin/train/easy_colab.ipynb to colab.ipynb

--- bin/train/main.py
+++
@@ -1,9 +0,0 @@
-# File: train.py
-# Created Date: Saturday February 5th 2022
-# Author: Steven Atkinson (steven@atkinson.mn)
-
-from nam.train.cli import run
-
-
-if __name__ == "__main__":
-    run()
