8e838ceb055548f4ede174e015c5702f858f45c5
Steven Atkinson <steven@atkinson.mn>
2024-12-02 18:30:40 -0800

    Test for longer-than-65,536 inputs for all model types

--- tests/test_nam/test_models/base.py
+++ tests/test_nam/test_models/base.py
@@ -2,12 +2,15 @@
 # Created Date: Saturday June 4th 2022
 # Author: Steven Atkinson (steven@atkinson.mn)
 
-import abc
-from pathlib import Path
-from tempfile import TemporaryDirectory
+import abc as _abc
+from pathlib import Path as _Path
+from tempfile import TemporaryDirectory as _TemporaryDirectory
+
+import pytest as _pytest
+import torch as _torch
 
 
-class Base(abc.ABC):
+class Base(_abc.ABC):
     @classmethod
     def setup_class(cls, C, args=None, kwargs=None):
         cls._C = C
@@ -20,8 +23,42 @@ class Base(abc.ABC):
 
     def test_export(self, args=None, kwargs=None):
         model = self._construct(args=args, kwargs=kwargs)
-        with TemporaryDirectory() as tmpdir:
-            model.export(Path(tmpdir))
+        with _TemporaryDirectory() as tmpdir:
+            model.export(_Path(tmpdir))
+
+    @_pytest.mark.parametrize(
+        "device",
+        (
+            _pytest.param(
+                "cuda",
+                marks=_pytest.mark.skipif(
+                    not _torch.cuda.is_available(), reason="CUDA-specific test"
+                ),
+            ),
+            _pytest.param(
+                "mps",
+                marks=_pytest.mark.skipif(
+                    not _torch.backends.mps.is_available(), reason="MPS-specific test"
+                ),
+            ),
+        ),
+    )
+    def test_process_input_longer_than_65536_on(self, device: str):
+        """
+        Processing inputs longer than 65,536 samples using various accelerator
+        backends can cause problems.
+
+        See:
+        * https://github.com/sdatkinson/neural-amp-modeler/issues/505
+        * https://github.com/sdatkinson/neural-amp-modeler/issues/512
+
+        (Funny that both have the same length limit--65,536...)
+
+        Assert that precautions are taken.
+        """
+        x = _torch.zeros((65_536 + 1,)).to(device)
+        model = self._construct().to(device)
+        model(x)
 
     def _construct(self, C=None, args=None, kwargs=None):
         C = self._C if C is None else C
