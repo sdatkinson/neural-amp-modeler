98d9ee8babb12b34072801c46b1abecc35b64b66
Steven Atkinson <steven@atkinson.mn>
2025-02-15 10:09:23 -0800

    Add hello world CLI entry point to verify installation

--- nam/cli.py
+++ nam/cli.py
@@ -86,6 +86,22 @@ from nam.train.gui import run as nam_gui  # noqa F401 Used as an entry point
 from nam.util import timestamp as _timestamp
 
 
+def nam_hello_world():
+    """
+    This is a minimal CLI entry point that's meant to be used to ensure that NAM
+    was installed successfully
+    """
+    from nam import __version__
+    msg = f"""
+    Neural Amp Modeler
+
+    by Steven Atkinson
+
+    Version {__version__}
+    """
+    print(msg)
+
+
 def nam_full():
     parser = _ArgumentParser()
     parser.add_argument("data_config_path", type=str)
