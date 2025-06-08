e45738f2ea302eb57cc5d50ad4e4201b01e3fe89
Steven Atkinson <steven@atkinson.mn>
2025-06-08 12:34:03 -0700

    Move auraloss to AAM fork, add test for MPS. Fails on prev main, passes here

--- tests/test_nam/test_models/test_losses.py
+++ tests/test_nam/test_models/test_losses.py
@@ -2,33 +2,35 @@
 # Created Date: Saturday January 28th 2023
 # Author: Steven Atkinson (steven@atkinson.mn)
 
-import pytest
-import torch
-import torch.nn as nn
+import pytest as _pytest
+import torch as _torch
+import torch.nn as _nn
 
-from nam.models import losses
+from nam.models import losses as _losses
+
+from ..._skips import requires_mps as _requires_mps
 
 
-@pytest.mark.parametrize(
+@_pytest.mark.parametrize(
     "x,coef,y_expected",
     (
-        (torch.Tensor([0.0, 1.0, 2.0]), 1.0, torch.Tensor([1.0, 1.0])),
-        (torch.Tensor([0.0, 1.0, 2.0]), 0.5, torch.Tensor([1.0, 1.5])),
+        (_torch.Tensor([0.0, 1.0, 2.0]), 1.0, _torch.Tensor([1.0, 1.0])),
+        (_torch.Tensor([0.0, 1.0, 2.0]), 0.5, _torch.Tensor([1.0, 1.5])),
         (
-            torch.Tensor([[0.0, 1.0, 0.0], [1.0, 1.5, 2.0]]),
+            _torch.Tensor([[0.0, 1.0, 0.0], [1.0, 1.5, 2.0]]),
             0.5,
-            torch.Tensor([[1.0, -0.5], [1.0, 1.25]]),
+            _torch.Tensor([[1.0, -0.5], [1.0, 1.25]]),
         ),
     ),
 )
 def test_apply_pre_emphasis_filter_1d(
-    x: torch.Tensor, coef: float, y_expected: torch.Tensor
+    x: _torch.Tensor, coef: float, y_expected: _torch.Tensor
 ):
-    y_actual = losses.apply_pre_emphasis_filter(x, coef)
-    assert isinstance(y_actual, torch.Tensor)
+    y_actual = _losses.apply_pre_emphasis_filter(x, coef)
+    assert isinstance(y_actual, _torch.Tensor)
     assert y_actual.ndim == y_expected.ndim
     assert y_actual.shape == y_expected.shape
-    assert torch.allclose(y_actual, y_expected)
+    assert _torch.allclose(y_actual, y_expected)
 
 
 def test_esr():
@@ -36,28 +38,37 @@ def test_esr():
     Is the ESR calculation correct?
     """
 
-    class Model(nn.Module):
+    class Model(_nn.Module):
         def forward(self, x):
             return x
 
     batch_size, input_length = 3, 5
     inputs = (
-        torch.linspace(0.1, 1.0, batch_size)[:, None]
-        * torch.full((input_length,), 1.0)[None, :]
+        _torch.linspace(0.1, 1.0, batch_size)[:, None]
+        * _torch.full((input_length,), 1.0)[None, :]
     )  # (batch_size, input_length)
-    target_factor = torch.linspace(0.37, 1.22, batch_size)
+    target_factor = _torch.linspace(0.37, 1.22, batch_size)
     targets = target_factor[:, None] * inputs  # (batch_size, input_length)
     # Do the algebra:
     # y=a*yhat
     # ESR=(y-yhat)^2 / y^2
     # ...
     # =(1/a-1)^2
-    expected_esr = torch.square(1.0 / target_factor - 1.0).mean()
+    expected_esr = _torch.square(1.0 / target_factor - 1.0).mean()
     model = Model()
     preds = model(inputs)
-    actual_esr = losses.esr(preds, targets)
-    assert torch.allclose(actual_esr, expected_esr)
+    actual_esr = _losses.esr(preds, targets)
+    assert _torch.allclose(actual_esr, expected_esr)
+
+
+@_requires_mps
+def test_mrstft_loss_doesnt_fall_back_to_cpu():
+    preds, targets = _torch.randn((2, 2048))
+    # Implicitly assert that this doesn't raise an exception that's usually caught and
+    # fallback
+    # If this raises a NotImplementedError, then that's what we don't want!
+    _losses.multi_resolution_stft_loss(preds, targets, device="mps")
 
 
 if __name__ == "__main__":
-    pytest.main()
+    _pytest.main()
