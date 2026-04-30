e296039fb46607a87e8741325687d53ba204fcc1
Steven Atkinson <steven@atkinson.mn>
2026-04-30 12:07:05 -0700

    [SECURITY] Update pytorch_lightning dependency to exclude specific versions (#657)
    * Update pytorch_lightning dependency to exclude specific versions
    
    * Update docs version to 0.12.3
    
    * Test to ensure dependency is safe (#626)
    
    * Remove deprecated test for specific pytorch_lightning versions and add pytest options to configuration
    
    * Add link to issue in pytest_configure docstring for compromised PyTorch Lightning versions
    
    * Pin to <=2.6.1

---
+++ conftest.py
@@ -0,0 +1,25 @@
+from importlib.metadata import PackageNotFoundError, version
+
+import pytest
+
+
+_COMPROMISED_PYTORCH_LIGHTNING_VERSIONS = {"2.6.2", "2.6.3"}
+
+
+def pytest_configure(config):
+    """
+    Block test execution if a known-compromised PyTorch Lightning version is installed.
+
+    See: https://github.com/sdatkinson/neural-amp-modeler/issues/656
+    """
+    try:
+        pl_version = version("pytorch_lightning")
+    except PackageNotFoundError:
+        return
+
+    if pl_version in _COMPROMISED_PYTORCH_LIGHTNING_VERSIONS:
+        pytest.exit(
+            "Blocked compromised pytorch_lightning version "
+            f"{pl_version}. Uninstall immediately and upgrade.",
+            returncode=2,
+        )
